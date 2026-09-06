a00b86f91bc32fd135c0382db2ef707f6f710d25
MinhHan <han@minhhan.net>
2026-09-06 17:05:15 +0700

    style: fix optgroup background color in dark mode

---
+++ fixSelectCssRegex.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let css = fs.readFileSync('frontend/src/admin.css', 'utf8');
+css = css.replace(/\.input option\s*\{[\s\S]*?\}/, (match) => {
+  return match + "\n.input optgroup {\n  background: var(--sidebar-bg);\n  color: var(--text-muted);\n  font-weight: 600;\n  font-style: normal;\n}";
+});
+fs.writeFileSync('frontend/src/admin.css', css, 'utf8');
